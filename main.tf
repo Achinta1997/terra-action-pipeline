c332c3d133f249ddf9e7ece8d7fb8a70ced14413
Achinta <achintadutta1997@gmail.com>
2025-08-24 18:38:05 +0530

    code added

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0"
+    }
+  }
+
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "661ea49e-05d4-429e-8204-de0d46c539b5"
+}
+
+resource "azurerm_resource_group" "RG" {
+  name     = "github-actions-rg"
+  location = "West Europe" 
+}
