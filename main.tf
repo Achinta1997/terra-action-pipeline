ea483ec40184d7904163cd6a23082eac750769fe
Achinta <achintadutta1997@gmail.com>
2025-09-02 19:58:49 +0530

    formatting done

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-backend "azurerm" {
-    resource_group_name   = "achinta-dev"
-    storage_account_name  = "terrastorage1"
-    container_name        = "terra-remote"
-    key                   = "terraformaction.tfstate"
-}
+  backend "azurerm" {
+    resource_group_name  = "achinta-dev"
+    storage_account_name = "terrastorage1"
+    container_name       = "terra-remote"
+    key                  = "terraformaction.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -20,10 +20,10 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "RG" {
   name     = "github-actions-rg"
-  location = "West Europe" 
+  location = "West Europe"
 }
 
 resource "azurerm_resource_group" "RG1" {
   name     = "github-actions-rg1"
-  location = "West Europe" 
+  location = "West Europe"
 }
